22fd1ad30efd5093242008253605470219410ed4
Amanuel Nigussie <amannugussie@gmail.com>
2022-04-21 17:34:32 -0600

    Schema draft

---
+++ server/models/standSchema.js
@@ -0,0 +1,35 @@
+const mongoose=require('mongoose');
+
+const localStandsSchema = new mongoose.Schema({
+    type: String,
+    location_name :String,
+    address : String,
+    hours:String,
+    duration: String,
+    community: String,
+    sector :String,
+    vendor_name :String,
+    vendor_description :String,
+    product_description :String,
+    // create_dt_utc: fixed_timestamp,
+    // mod_dt_utc :fixed_timestamp,
+    // point :Point
+});
+
+const citySchema = new mongoose.Schema({
+ name: String,
+ location: {
+   type: {
+     type: String, 
+     enum: ['Point'], 
+     required: true
+   },
+   coordinates: {
+     type: [Number],
+     required: true
+   }
+ }
+});
+
+module.exports = mongoose.model('City', citySchema);
+module.exports = mongoose.model('localStands', localStandsSchema);

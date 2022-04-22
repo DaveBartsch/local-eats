9fe9668ccbc854b5125f96e30a8d1dd4186dc47d
David <dave.bartsch@icloud.com>
2022-04-22 11:01:51 -0600

    fixed schema variable names. exported Stand/City as variables

--- server/models/standSchema.js
+++ server/models/standSchema.js
@@ -1,35 +1,38 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const localStandsSchema = new mongoose.Schema({
-    type: String,
-    location_name :String,
-    address : String,
-    hours:String,
-    duration: String,
-    community: String,
-    sector :String,
-    vendor_name :String,
-    vendor_description :String,
-    product_description :String,
-    // create_dt_utc: fixed_timestamp,
-    // mod_dt_utc :fixed_timestamp,
-    // point :Point
+const standSchema = new mongoose.Schema({
+  type: String,
+  location_name: String,
+  address: String,
+  hours: String,
+  duration: String,
+  community: String,
+  sector: String,
+  vendor_name: String,
+  vendor_description: String,
+  product_description: String,
+  // create_dt_utc: fixed_timestamp,
+  // mod_dt_utc :fixed_timestamp,
+  // point :Point
 });
 
-const City = new mongoose.Schema({
- name: String,
- location: {
-   type: {
-     type: String, 
-     enum: ['Point'], 
-     required: true
-   },
-   coordinates: {
-     type: [Number],
-     required: true
-   }
- }
+const citySchema = new mongoose.Schema({
+  name: String,
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
 });
 
-module.exports = mongoose.model('City', citySchema);
-module.exports = mongoose.model('localStands', localStandsSchema);
+const Stand = mongoose.model("Stand", standSchema);
+const City = mongoose.model("City", citySchema);
+
+module.exports = Stand;
+module.exports = City;

49279d2853f3c943d176cf6b619f33071fcd3d90
David <dave.bartsch@icloud.com>
2022-04-25 19:45:44 -0600

    added comments for app.js

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
-var createError = require("http-errors");
+// import express
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
+
+// declare express as app
+var app = express();
+
+// import logger: morgan
 var logger = require("morgan");
 
+//added by default from npx express-generator
+var path = require("path");
+var cookieParser = require("cookie-parser");
+// var createError = require("http-errors");
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -16,18 +21,18 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//added by default from npx express-generator
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 // app.use(function (req, res, next) {
 //   next(createError(404));
 // });
 
-// error handler
+// error handler (added by default)
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;

d3b9fe83ef5f81cfb59e070a99be6a69c75e6467
David <dave.bartsch@icloud.com>
2022-04-27 14:45:44 -0600

    changed mongoose.connect back

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,13 +5,9 @@ const mongoose = require("mongoose");
 const connectionString = "mongodb://localhost:27017/yycLocalEats";
 
 // connect mongoose to connectionString
-try {
-  mongoose.connect(connectionString, () => {
-    console.log(`Connected to Mongoose on: ${connectionString}`);
-  });
-} catch (err) {
-  console.log(`MongoDB Connection Failed!  err is ${err}`);
-}
+mongoose.connect(connectionString, () => {
+  console.log(`Connected to Mongoose on: ${connectionString}`);
+});
 
 // export mongoose
 module.exports = mongoose;

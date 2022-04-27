a90cae869adf16c6f20d68df8585a1617203467f
David <dave.bartsch@icloud.com>
2022-04-27 17:51:06 -0600

    changed shema: vendor_name to top & type to bottom

--- server/db/models/standSchema.js
+++ server/db/models/standSchema.js
@@ -3,16 +3,16 @@ const mongoose = require("../mongoose");
 
 // schema for farm stand
 const farmStandSchema = new mongoose.Schema({
-  type: String,
+  vendor_name: String,
   location_name: String,
   address: String,
   hours: String,
   duration: String,
   community: String,
   sector: String,
-  vendor_name: String,
   vendor_description: String,
   product_description: String,
+  type: String,
   // create_dt_utc: fixed_timestamp,
   // mod_dt_utc :fixed_timestamp,
   // point :Point

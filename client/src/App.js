274c809d1c9e6d001d9d4300bbf6824625adbb0c
Amanuel Nigussie <amannugussie@gmail.com>
2022-04-28 09:23:43 -0600

    imported NewFarmStand Component, created showModal states, and applied showed modal on Add a new farm stand Button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import FarmStandData from "./components/FarmStandData";
 import { useState, useEffect } from "react";
+import NewFarmStandForm from "./components/NewFarmStandForm";
 
 function App() {
   const [farmStandList, setFarmStandList] = useState();
+  const [showModal, setShowModal] = useState(false);
   console.log(`Farm Stands list: ${JSON.stringify(farmStandList)}`);
 
   useEffect(() => {
@@ -20,21 +22,29 @@ function App() {
       console.log(ex);
     }
   };
-   if(farmStandList===undefined){
-    return <div>Loading...</div>
-   } else {
-
-  return (
-    <div className="App">
-      <header>
-        <h1>Farm Stands</h1>
-      </header>
-      { farmStandList.map((farmStand) => {
-        return <FarmStandData farmStand={farmStand} />;
-      })}
-    </div>
-  );
-    }
+  if (farmStandList === undefined) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App">
+        <header>
+          <h1>Farm Stands</h1>
+        </header>
+        {farmStandList.map((farmStand) => {
+          return <FarmStandData farmStand={farmStand} />;
+        })}
+        <button
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          {" "}
+          Add a new FarmStand
+        </button>
+        {showModal && <NewFarmStandForm setShowModal={setShowModal} />}
+      </div>
+    );
+  }
 }
 
 export default App;

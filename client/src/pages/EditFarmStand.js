2715ccbace9856d9747b84ec2c8fb651321f7f83
Amanuel Nigussie <amannugussie@gmail.com>
2022-04-30 00:21:09 -0600

    added a navigate react dom method to return back the edited farm stand and when  edit button is clicked if 200 it will display  the edited one.

--- client/src/pages/EditFarmStand.js
+++ client/src/pages/EditFarmStand.js
@@ -1,50 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import NewFarmStandForm from "../components/NewFarmStandForm";
-
-
 
 const EditFarmStand = () => {
   //const params = useParams();
-  const 
-  { id } = useParams();
+  const { id } = useParams();
   //console.log(`params: ${params}`);
   //const id = params.id;
+  const navigate=useNavigate();
   console.log(`id: ${id}`);
   console.log(`Editing Farm Stand ${id}`);
   const [farmStand, setFarmStand] = useState();
-  
+
   useEffect(() => {
     const getFarmStand = async () => {
       let response = await fetch(`/farm_stands/${id}`);
       let data = await response.json();
-      setTimeout(()=> setFarmStand(data),1000);
+      setTimeout(() => setFarmStand(data), 1000);
       console.log(data);
     };
     getFarmStand();
   }, [id]);
 
-   const onFormSubmit = async (farmStand) => { 
-       const data=JSON.stringify(farmStand);
-       console.log(data);
+  const onFormSubmit = async (farmStand) => {
+    const data = JSON.stringify(farmStand);
+    console.log(data);
     let response = await fetch(`/farm_stands/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: data,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
     });
-    if(response.status===200){
-        console.log("Edited successfully");
+    if (response.status === 200) {
+      console.log("Edited successfully");
+    setTimeout(()=>{  navigate(`/details/${id}`)}, 1000);
     } else {
-        alert("Error editing farm stand");
+      alert("Error editing farm stand");
     }
-    };
+  };
 
   return (
     <div>
       <h2>Edit Farm Stand</h2>
-      <NewFarmStandForm  initialValues={farmStand} onFormSubmit={onFormSubmit}/>
+      <NewFarmStandForm
+        initialValues={farmStand}
+        onFormSubmit={onFormSubmit}
+        buttonText="Update a Farm Stand"
+      />
     </div>
   );
 };

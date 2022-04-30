69e8e7abcc8b2c43026128d4280cf017f6e49703
Amanuel Nigussie <amannugussie@gmail.com>
2022-04-29 23:54:26 -0600

    set the NewFarmStand  farmStand Value to initialValue props to get all va;ues

--- client/src/pages/EditFarmStand.js
+++ client/src/pages/EditFarmStand.js
@@ -2,23 +2,31 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import NewFarmStandForm from "../components/NewFarmStandForm";
 
-const EditFarmStand = () => {
-  const params = useParams();
 
-  const id = params.id;
+
+const EditFarmStand = () => {
+  //const params = useParams();
+  const 
+  { id } = useParams();
+  //console.log(`params: ${params}`);
+  //const id = params.id;
+  console.log(`id: ${id}`);
+  console.log(`Editing Farm Stand ${id}`);
   const [farmStand, setFarmStand] = useState();
   
   useEffect(() => {
     const getFarmStand = async () => {
       let response = await fetch(`/farm_stands/${id}`);
       let data = await response.json();
-      setFarmStand(data);
+      setTimeout(()=> setFarmStand(data),1000);
+      console.log(data);
     };
     getFarmStand();
   }, [id]);
 
    const onFormSubmit = async (farmStand) => { 
        const data=JSON.stringify(farmStand);
+       console.log(data);
     let response = await fetch(`/farm_stands/${id}`, {
         method: "PUT",
         headers: {
@@ -36,7 +44,7 @@ const EditFarmStand = () => {
   return (
     <div>
       <h2>Edit Farm Stand</h2>
-      <NewFarmStandForm  farmStand={farmStand} initialValues={onFormSubmit}/>
+      <NewFarmStandForm  initialValues={farmStand} onFormSubmit={onFormSubmit}/>
     </div>
   );
 };

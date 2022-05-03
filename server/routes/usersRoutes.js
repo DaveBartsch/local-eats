09ce54ee9aa3e5697f1db6217e657f6d7284f6a9
Amanuel Nigussie <amannugussie@gmail.com>
2022-05-03 13:55:00 -0600

    userRoute for verification and login route

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -9,18 +9,30 @@ const {
   updateUser,
   verifyPassword
 } = require("../db/models/User");
+const { route } = require("express/lib/application");
 
 const LocalStrategy = require("passport-local").Strategy;
 
 passport.use(async function LocalStrategy(username, password, done) {
+    try{
     const user =await getUserByName(username);
     if (!user) {
         return done(null, false, { message: "Incorrect username." });
-    }
+    } 
     const isMatch = await verifyPassword(password, user.password);
     if (!isMatch) {
         return done(null, false, { message: "Incorrect password." });
     }
-    return done(null, user);
+    return done(null, user);}
+    catch(err){
+        return done(error,`Error: ${err}`);// other error apart from user name and password error
+    }
 });
 
+route.post('/login',passport.authenticate("local"),(req,res)=>{
+    res.send(req.user);
+    res.send("Logged in");
+    res.send("success");
+});
+
+    )

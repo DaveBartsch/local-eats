d4a7562ccb7d72a4d8c8ca0390230b3246d25a58
Amanuel Nigussie <amannugussie@gmail.com>
2022-05-03 00:23:59 -0600

    Inserted Topography

--- client/src/components/FarmStandsList.jsx
+++ client/src/components/FarmStandsList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import FarmStandData from "./FarmStandData";
+import { Grid, Typography } from "@mui/material";
 
 const FarmStandsList = () => {
   const [farmStandList, setFarmStandList] = useState();
@@ -18,20 +19,24 @@ const FarmStandsList = () => {
   }, []);
 
   return (
-    <div>
-      
-      {
-    farmStandList ? (
-      farmStandList.map((farmStand) => {
-        return <FarmStandData farmStand={farmStand}  />;
-      })
-    ) : (
-      <div>Loading...</div>
-    )
-  }
-    </div>
-  
- )
+    <Grid container>
+      {farmStandList ? (
+        farmStandList.map((farmStand) => {
+          return (
+            
+              <Grid xs={12} sm={6} md={3} item>
+                <FarmStandData farmStand={farmStand} />
+              </Grid>
+            
+          );
+        })
+      ) : (
+        <Typography Component="div" variant="h3">
+          Loading...
+        </Typography>
+      )}
+    </Grid>
+  );
 };
 
 export default FarmStandsList;

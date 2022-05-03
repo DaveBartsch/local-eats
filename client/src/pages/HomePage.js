492f3fd68de6966c82923f9c93f0a51fb1ee5e5e
Amanuel Nigussie <amannugussie@gmail.com>
2022-05-03 00:26:52 -0600

    Created Typography

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,10 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import FarmStandsList from "../components/FarmStandsList";
 const HomePage = () => {
   return (
     <div>
-        <h2>Calgary FarmStands List</h2>
+        <Typography variant="h3">Calgary FarmStands List</Typography>
       <FarmStandsList />
     </div>
   );

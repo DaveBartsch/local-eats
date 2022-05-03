3b7b1aea07710762ed93da2b2d09c3429c8c2a37
Amanuel Nigussie <amannugussie@gmail.com>
2022-05-03 12:02:14 -0600

    Made a user Shema

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,5 +1,5 @@
 const mongoose=require ('mongoose');
-
+const bcrypt=require('bcrypt');
 
 const UserSchema=new mongoose.Schema({
     userName:{  
@@ -29,3 +29,37 @@ const UserSchema=new mongoose.Schema({
 );  
 
 module.export=mongoose.model('User',UserSchema);
+
+
+    //hash password
+    const createUser=(user)=>{
+    const hashedPassword=await bcrypt.hash(user.password,10);
+    const newUser=await user.create({
+        userName:user.userName,
+        email:user.email,
+        password:hashedPassword
+    });
+    return newUser;
+}
+
+const getUserById=(id)=>{
+    const   user=user.findById(id);
+    return user;
+}
+
+const updateUser=(id,updateduser)=>{
+    const user=user.findByIdAndUpdate(id,updateduser);
+    return true;
+}
+
+const deleteUser=(id)=>{
+    const user=user.findByIdAndDelete(id);
+    return true;
+}
+
+module.exports={
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser
+}

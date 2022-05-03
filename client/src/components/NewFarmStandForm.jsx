32f3e9eb60cb67558441168047c64dea7c804c9d
Amanuel Nigussie <amannugussie@gmail.com>
2022-05-03 00:24:45 -0600

    Inserted Material Icon and Buttons

--- client/src/components/NewFarmStandForm.jsx
+++ client/src/components/NewFarmStandForm.jsx
@@ -1,8 +1,18 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import {
+  Container,
+  TextField,
+  Typography,
+  Box,
+  List,
+  ListItem,
+  ListSubheader,
+  Button,
+} from "@mui/material";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 const NewFarmStandForm = (props) => {
-   
   const [type, setType] = useState("");
   const [location_name, setLocation_name] = useState("");
   const [address, setAddress] = useState("");
@@ -13,10 +23,12 @@ const NewFarmStandForm = (props) => {
   const [vendor_name, setVendor_name] = useState("");
   const [vendor_description, setVendor_description] = useState("");
   const [product_description, setProduct_description] = useState("");
-  const setShowModal=props.setShowModal;
-  const onFormSubmit=props.onFormSubmit;
-  const initialValues=props.initialValues;
-  const buttonText=props.buttonText;
+  const setShowModal = props.setShowModal;
+  const onFormSubmit = props.onFormSubmit;
+  const initialValues = props.initialValues;
+  const buttonText = props.buttonText;
+  const onFormCancel=props.onFormCancel;
+  const onFormDelete=props.onFormDelete;
 
   useEffect(() => {
     if (initialValues) {
@@ -32,131 +44,143 @@ const NewFarmStandForm = (props) => {
       setProduct_description(initialValues.product_description);
       console.log(`initialValues is:`, initialValues);
     }
-  
   }, [initialValues]);
-    
-  
-
 
   return (
-    <div>
-      <h2>Add a new FarmStand</h2>
-      <div>
-        <label>FarmStand Type:</label>
-        <input
-          type="text"
-          name="type"
-          value={type}
-          onChange={(event) => {
-            setType(event.target.value);
-          }}
-        />
-        <br />
-        <label>Location Name:</label>
-        <input
-          type="text"
-          name="location_name"
-          value={location_name}
-          onChange={(event) => {
-            setLocation_name(event.target.value);
-          }}
-        />
-        <br />
-        <label>Address:</label>
-        <input
-          type="text"
-          name="address"
-          value={address}
-          onChange={(event) => {
-            setAddress(event.target.value);
-          }}
-        />
-        <br />
-        <label>Hours:</label>
-        <input
-          type="text"
-          name="hours"
-          value={hours}
-          onChange={(event) => {
-            setHours(event.target.value);
-          }}
-        />
-        <br />
-        <label>Duration:</label>
-        <input
-          type="text"
-          name="duration"
-          value={duration}
-          onChange={(event) => {
-            setDuration(event.target.value);
-          }}
-        />
-        <br />
-        <label>Community:</label>
-        <input
-          type="text"
-          name="community"
-          value={community}
-          onChange={(event) => {
-            setCommunity(event.target.value);
-          }}
-        />
-        <br />
-        <label>Sector:</label>
-        <input
-          type="text"
-          name="sector"
-          value={sector}
-          onChange={(event) => {
-            setSector(event.target.value);
-          }}
-        />
-        <br />
-        <label>Vendor Name:</label>
-        <input
-          type="text"
-          name="vendor_name"
-          value={vendor_name}
-          onChange={(event) => {
-            setVendor_name(event.target.value);
-          }}
-        />
-        <br />
-        <label>Vendor Description:</label>
-        <input
-          type="text"
-          name="vendor_description"
-          value={vendor_description}
-          onChange={(event) => {
-            setVendor_description(event.target.value);
-          }}
-        />
-        <br />
-        <label>Product Description:</label>
-        <input
-          type="text"
-          name="product_description"
-          value={product_description}
-          onChange={(event) => {
-            setProduct_description(event.target.value);
-          }}
-        />
-        <br />
-        <button onClick={()=>onFormSubmit({
-          type:type,
-          location_name:location_name,
-          address:address,
-          hours:hours,
-          duration:duration,
-          community:community,
-          sector:sector,
-          vendor_name:vendor_name,
-          vendor_description:vendor_description,
-          product_description:product_description
-          
-        })}>{buttonText}</button>
-      </div>
-    </div>
+    <Container sx={{ mt: 4 }}>
+      <Box display="flex" flexDirection="column" mb={3}>
+        <div>
+          <label>FarmStand Type:</label>
+          <TextField
+            label="FarmStand Type"
+            type="text"
+            variant="standard"
+            backgroundColor="light green"
+            name="type"
+            value={type}
+            onChange={(event) => {
+              setType(event.target.value);
+            }}
+          />
+          <br />
+          <label>Location Name:</label>
+          <TextField
+            label="Location Name"
+            type="text"
+            variant="standard"
+            backgroundColor="light green"
+            name="location_name"
+            value={location_name}
+            onChange={(event) => {
+              setLocation_name(event.target.value);
+            }}
+          />
+          <br />
+          <label>Address:</label>
+          <input
+            type="text"
+            name="address"
+            value={address}
+            onChange={(event) => {
+              setAddress(event.target.value);
+            }}
+          />
+          <br />
+          <label>Hours:</label>
+          <input
+            type="text"
+            name="hours"
+            value={hours}
+            onChange={(event) => {
+              setHours(event.target.value);
+            }}
+          />
+          <br />
+          <label>Duration:</label>
+          <input
+            type="text"
+            name="duration"
+            value={duration}
+            onChange={(event) => {
+              setDuration(event.target.value);
+            }}
+          />
+          <br />
+          <label>Community:</label>
+          <input
+            type="text"
+            name="community"
+            value={community}
+            onChange={(event) => {
+              setCommunity(event.target.value);
+            }}
+          />
+          <br />
+          <label>Sector:</label>
+          <input
+            type="text"
+            name="sector"
+            value={sector}
+            onChange={(event) => {
+              setSector(event.target.value);
+            }}
+          />
+          <br />
+          <label>Vendor Name:</label>
+          <input
+            type="text"
+            name="vendor_name"
+            value={vendor_name}
+            onChange={(event) => {
+              setVendor_name(event.target.value);
+            }}
+          />
+          <br />
+          <label>Vendor Description:</label>
+          <input
+            type="text"
+            name="vendor_description"
+            value={vendor_description}
+            onChange={(event) => {
+              setVendor_description(event.target.value);
+            }}
+          />
+          <br />
+          <label>Product Description:</label>
+          <input
+            type="text"
+            name="product_description"
+            value={product_description}
+            onChange={(event) => {
+              setProduct_description(event.target.value);
+            }}
+          />
+          <br />
+          <Button
+            variant="contained"
+            variantColor="green"
+            onClick={() =>
+              onFormSubmit({
+                type: type,
+                location_name: location_name,
+                address: address,
+                hours: hours,
+                duration: duration,
+                community: community,
+                sector: sector,
+                vendor_name: vendor_name,
+                vendor_description: vendor_description,
+                product_description: product_description,
+              })
+            }
+          >
+            {buttonText}
+          </Button>
+          <Button onClick={onFormCancel}>Cancel</Button>
+          <Button onClick={onFormDelete}>Delete</Button>
+        </div>
+      </Box>
+    </Container>
   );
 };
 

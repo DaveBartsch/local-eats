aed408c62aa464f177a1bf1c00486624a037771d
Amanuel Nigussie <amannugussie@gmail.com>
2022-05-04 05:21:12 -0600

    updated mongo connection settings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,28 @@
-// import mongoose
-const mongoose = require("mongoose");
+// // import mongoose
+// const mongoose = require("mongoose");
 
-// new variable: connection string
-const connectionString = "mongodb://localhost:27017/yycLocalEats";
+// // new variable: connection string
+// const connectionString = "mongodb://127.0.0.1:27017/yycLocalEats";
 
-// connect mongoose to connectionString
-mongoose.connect(connectionString, () => {
-  console.log(`Connected to Mongoose on: ${connectionString}`);
-});
+// // connect mongoose to connectionString
+// mongoose.connect(connectionString, () => {
+//   console.log(`Connected to Mongoose on: ${connectionString}`);
+// });
 
 // export mongoose
+
+
+const mongoose = require("mongoose");
+
+const uri = "mongodb://localhost/yycLocalEats";
+
+mongoose.connect(uri).catch(err => {
+  console.log(`Error connecting to Mongoose: ${err.message}`)});
+
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("MongoDB database connection established successfully");
+});
+
 module.exports = mongoose;

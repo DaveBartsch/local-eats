8e3825c11908ce826f09ea0c1b91962e488ee299
David <dave.bartsch@icloud.com>
2022-05-04 17:05:59 -0600

    Added embedded map to landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import LandingInfo from "../components/landing-components/LandingInfo";
 import LandingSectors from "../components/landing-components/LandingSectors";
@@ -11,6 +12,7 @@ const LandingPage = () => {
         src="https://mediaproxy.salon.com/width/1200/https://media.salon.com/2021/08/farmers-market-produce-0812211.jpg"
         alt="http://farmgrants.ca/wp-content/uploads/2018/04/vegetables-basket_fyE57FBd_L-1024x681.jpg"
       />
+      <h1> Calgary Farm Stands:</h1>
       <MapComponent />
       <LandingSectors />
       <LandingInfo />

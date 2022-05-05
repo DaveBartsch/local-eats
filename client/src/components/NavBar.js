9cd5a264df5351d9a4c6e397962f7158471f6945
David <dave.bartsch@icloud.com>
2022-05-05 17:42:46 -0600

    Added green backgrounds

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,26 +4,54 @@ import Button from "@mui/material/Button";
 import { Link as RouterLink } from "react-router-dom";
 import { Link } from "@mui/material";
 export default function NavBar(props) {
-   const { loggedInUser } = props;
-   const isAgent=loggedInUser?.isAgent
- return (
- <AppBar position="static" style={{ background: '#013220' }}>
- <Toolbar>
- <Link component={RouterLink} color="white" underline="none" to="/">
- <Button color="inherit">Home</ Button>
- </Link>
- {isAgent && (<Link component={RouterLink} color="white" underline="none" to="/add">
-    
- <Button color="inherit">Create a FarmStand</ Button>
- </Link>)}
- {!loggedInUser && <Link component={RouterLink} color="white" underline="none" to="/login">
-    <Button color="inherit">Login</ Button>
-  
- </Link>}
- {loggedInUser && <Link component={RouterLink} color="white" underline="none" to="/logout">
-    <Button color="inherit">Logout</ Button>
-    </Link>}
- </Toolbar>
- </AppBar>
- );
+  const { loggedInUser } = props;
+  const isAgent = loggedInUser?.isAgent;
+  return (
+    <AppBar
+      position="static"
+      style={{
+        backgroundImage: `url("https://cdn.w600.comps.canstockphoto.ca/seamless-pattern-with-different-fresh-clip-art-vector_csp36423268.jpg")`,
+      }}
+    >
+      <Toolbar>
+        <Link component={RouterLink} color="white" underline="none" to="/">
+          <Button variant="contained" padding="1rem" color="success">
+            Home
+          </Button>
+        </Link>
+        {isAgent && (
+          <Link component={RouterLink} color="white" underline="none" to="/add">
+            <Button variant="contained" padding="1rem" color="success">
+              Create a FarmStand
+            </Button>
+          </Link>
+        )}
+        {!loggedInUser && (
+          <Link
+            component={RouterLink}
+            color="white"
+            underline="none"
+            to="/login"
+            padding="1rem"
+          >
+            <Button variant="contained" padding="1rem" color="success">
+              Login
+            </Button>
+          </Link>
+        )}
+        {loggedInUser && (
+          <Link
+            component={RouterLink}
+            color="white"
+            underline="none"
+            to="/logout"
+          >
+            <Button variant="contained" padding="1rem" color="success">
+              Logout
+            </Button>
+          </Link>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 }

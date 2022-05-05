820165313dd3237d2fc873888ac8bc1f07e4e468
David <dave.bartsch@icloud.com>
2022-05-05 17:46:16 -0600

    added green backgrounds

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,12 +2,11 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import { Link as RouterLink } from "react-router-dom";
-import { Link, Typography } from "@mui/material";
+import { Link } from "@mui/material";
 export default function NavBar(props) {
   const { loggedInUser } = props;
   const isAgent = loggedInUser?.isAgent;
   return (
-<<<<<<< HEAD
     <AppBar
       position="static"
       style={{
@@ -25,16 +24,6 @@ export default function NavBar(props) {
             <Button variant="contained" padding="1rem" color="success">
               Create a FarmStand
             </Button>
-=======
-    <AppBar position="static" style={{ background: "#013220" }}>
-      <Toolbar>
-        <Link component={RouterLink} color="white" underline="none" to="/">
-          <Button color="inherit">Home</Button>
-        </Link>
-        {isAgent && (
-          <Link component={RouterLink} color="white" underline="none" to="/add">
-            <Button color="inherit">Create a FarmStand</Button>
->>>>>>> 8ae7ac5635395f9b452b6ad1bd2ca959947e0c77
           </Link>
         )}
         {!loggedInUser && (
@@ -43,16 +32,11 @@ export default function NavBar(props) {
             color="white"
             underline="none"
             to="/login"
-<<<<<<< HEAD
             padding="1rem"
           >
             <Button variant="contained" padding="1rem" color="success">
               Login
             </Button>
-=======
-          >
-            <Button color="inherit">Login</Button>
->>>>>>> 8ae7ac5635395f9b452b6ad1bd2ca959947e0c77
           </Link>
         )}
         {loggedInUser && (
@@ -62,18 +46,11 @@ export default function NavBar(props) {
             underline="none"
             to="/logout"
           >
-<<<<<<< HEAD
             <Button variant="contained" padding="1rem" color="success">
               Logout
             </Button>
           </Link>
         )}
-=======
-            <Button color="inherit">Logout</Button>
-          </Link>
-        )}
-        <Typography align="center"> {loggedInUser?.username}</Typography>
->>>>>>> 8ae7ac5635395f9b452b6ad1bd2ca959947e0c77
       </Toolbar>
     </AppBar>
   );
